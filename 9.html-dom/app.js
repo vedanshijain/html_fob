9dd2a265d61baaacbb73af28fa48ff594d7ff369
vedanshijain <vedanshi.jain@indiamart.com>
2022-07-12 11:06:37 +0530

    final

---
+++ 9.html-dom/app.js
@@ -0,0 +1,8 @@
+document.getElementById("container").innerHTML="<h1>Welcome to DOM</h1>"
+document.getElementById("container").style.color ="blue"
+document.getElementById("container").style.backgroundColor ="yellow"
+document.getElementsByClassName("para")[0].innerHTML="<strong>This is paragraph</strong>"
+
+function clickMe(){
+    alert("did you click me ?")
+}
